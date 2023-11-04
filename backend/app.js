5afd67508fbea57d3fba45d2c06049ed5cfae8b3
Nikita Malenkiy <ma.ready.to.blow@gmail.com>
2023-11-04 20:45:48 +0300

    feature/add crash test

--- backend/app.js
+++ backend/app.js
@@ -29,6 +29,12 @@ app.use(express.urlencoded({ extended: true }));
 // app.use(cookieParser());
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(appRoute);
 
 app.use(errorLogger);
